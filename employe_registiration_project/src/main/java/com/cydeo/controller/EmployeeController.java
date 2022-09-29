58f007621356d2b02ef115baa0b76a684832e02a
aymehmet <aymehmet1976@gmail.com>
2022-09-30 00:53:41 +0300

    Bootstrap  Created

--- employe_registiration_project/src/main/java/com/cydeo/controller/EmployeeController.java
+++ employe_registiration_project/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.cydeo.controller;
 
 
+
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+        model.addAttribute("employee",new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
+
         return "employee/employee_create";
     }
 }
